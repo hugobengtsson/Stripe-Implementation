a180e7a22afc125cc5c8347c9bac596414388bcf
fredriklexo <91253151+fredriklexo@users.noreply.github.com>
2022-09-14 14:47:22 +0200

    verify and create order

--- controllers/paymentControllers.js
+++ controllers/paymentControllers.js
@@ -1,6 +1,7 @@
 import { stripe } from "../server.js";
+import  fs  from 'fs';
 
-
+const dataPath = './data/orders.json'
 
 export const createPayment = async (req, res) => {
     
@@ -8,6 +9,7 @@ export const createPayment = async (req, res) => {
         
         // loop över cartItems som blir line_items.
         const session = await stripe.checkout.sessions.create({
+            //fixa line items
             line_items: [
                 {
                     // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
@@ -23,6 +25,46 @@ export const createPayment = async (req, res) => {
         
         res.json(session.id);
         
+    }else{
+        res.status(401).json(false)
     }
 
+}
+
+export const verifyPayment = async (req, res) => {
+    try {
+        const findSession = await stripe.checkout.sessions.retrieve(req.body.sessionId)
+        if(findSession.payment_status == "paid"){
+            //pusha till order.json
+            const lineItems = await getLineItems(req.body.sessionId)
+            findSession.lineItems = lineItems.data
+            
+            let orders = fs.readFileSync(dataPath)
+            orders = JSON.parse(orders)
+            const findOrder = orders.find(order => order.id == findSession.id)
+            
+            if(!findOrder){
+                orders.push(findSession)
+                fs.writeFileSync(dataPath, JSON.stringify(orders))
+                
+            }
+            res.status(200).json("Success")
+            return 
+        }
+        res.status(404).json(false) //kolla statuskoden
+        
+    } catch (err) {
+        res.status(404).json(err.message)
+    }
+
+}
+
+export const getLineItems = async (sessionId) => {
+   try {
+
+    return await stripe.checkout.sessions.listLineItems(sessionId);
+
+   } catch (error) {
+    
+   }
 }
