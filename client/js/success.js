a256e8409a141082e040e68320af7cdcc02a1cb2
Hugo Bengtsson <hugo@capellium.se>
2022-09-15 14:52:31 +0200

    Bug on success.html fixed, reload to homepage

--- client/js/success.js
+++ client/js/success.js
@@ -1,17 +1,18 @@
-import { makeRequest } from "../helpers/fetchHelper.js";
+import { checkUserInCookie, logoutUser, makeRequest } from "../helpers/fetchHelper.js";
 
+let myPage = document.getElementById("myPage")
 
 const initSite = async () => {
-    
     const queryString = window.location.search;
     const urlParams = new URLSearchParams(queryString);
     const sessionId = urlParams.get('session_id')
- 
+    
     if(sessionId){
         await verifyPayment(sessionId)
     }else{
         window.location.href = "/" 
     }
+    showCorrectAuthBoxes()
 }
 
 async function verifyPayment(sessionId){
@@ -27,14 +28,36 @@ async function verifyPayment(sessionId){
     )
 
     const message = document.getElementById("message")
-    console.log(response);
-    if(response){
+
+    if(response.customer_details){
         localStorage.removeItem("cart")
         message.innerText = `Tack ${response.customer_details.name} för din beställning! Ditt ordernummer: ${response.id}`
         
     }else{
-        message.innerText = 'Din betalning misslyckades!'
+        window.location.href = "/" 
     }
 }
+
+async function showCorrectAuthBoxes() {
+    const checkuser = await checkUserInCookie();
+
+    if(checkuser.msg.user) {
+        myPage.innerText = "Logga ut"
+        myPage.href = "./index.html"
+        return
+    } 
+    window.location.href = "/" 
+}
+
+// What will happen when you click on the logOut-link
+myPage.addEventListener("click", async () => {
+    const checkuser = await checkUserInCookie();
+
+    if(checkuser.msg.user) {
+        await logoutUser()
+        alert("Du är utloggad!")
+        return
+    } 
+})
 
 window.addEventListener("load", initSite)
