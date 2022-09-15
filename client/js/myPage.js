8be97af0b175e62a086b6fbb71df09b856af942b
Angelica Moberg <angelica.moberg@hotmail.com>
2022-09-15 15:50:31 +0200

    user validation

--- client/js/myPage.js
+++ client/js/myPage.js
@@ -1,4 +1,5 @@
 import { checkUserInCookie, createUser, loginUser, getAllUsers } from "../helpers/fetchHelper.js"
+import validateValues from "../validation/validation.js"
 
 const buttonCA = document.querySelector(".buttonCA")
 const loginForm = document.querySelector("#login")
@@ -47,63 +48,21 @@ if(buttonCA) {
         const newCustomer = {
             name: fullName,
             email,
-            address,
-            zipcode,
-            city,
-            country,
+            address: {
+                line1: address,
+                postal_code: zipcode,
+                city,
+                country,
+            },
             phone,
             password
         }
 
-        if(
-            phone.length == 0 || 
-            fullName.length == 0 || 
-            confPassword.length == 0 || 
-            email.length == 0 || 
-            address.length == 0 || 
-            zipcode.length == 0 || 
-            country.length == 0 || 
-            city.length == 0) 
-        {
-            alert("Fyll i alla fält!")
-            return
-        }
+        let checkValues = validateValues(newCustomer)
 
-        if(!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(email)) {
-            alert(" Email - Fel format")
+        if(!checkValues.bool) {
+            alert(checkValues.msg)
             return
-        }
-        if(country == "Sverige" && zipcode.length != 5 || /^[0-9]+$/.test(zipcode) == false) {
-            alert("Postkod - Måste innehålla 5 siffror")
-            return
-        }
-        if(country == "Norge" && zipcode.length != 4 || /^[0-9]+$/.test(zipcode) == false) {
-            alert("Postkod - Måste innehålla 4 siffror")
-            return
-        }
-        if(phone.length != 10 || /^[0-9]+$/.test(phone) == false) {
-            alert("Telefonnummer -  Måste innehålla 10 siffror. Ex. 0721112233")
-            return
-        }
-        if(confPassword.length < 6) {
-            alert("Lösenord - Måste innehålla minst 6 karaktärer")
-            return
-        }
-        if(fullName.length < 3 ) {
-            alert("Namn - Måste innehålla minst 2 bokstäver")
-            return 
-        } 
-        if(/^[A-Za-z\s\u00C0-\u00ff_-]*$/.test(fullName) == false){
-            alert("Namn - Får endast innehålla bokstäver")
-            return 
-        }
-        if(/^[A-Za-z\s\u00C0-\u00ff_-]*$/.test(city) == false){
-            alert("Ort - Får endast innehålla bokstäver")
-            return 
-        }
-        if(city.length < 3) {
-            alert("Ort - Måste innehålla minst 2 bokstäver")
-            return 
         }
 
         const validPw = validPass(password, confPassword)
@@ -126,7 +85,6 @@ if(buttonCA) {
             alert(registerCustomer.msg)
             loginForm.classList.remove("hidden");
             createAccountForm.classList.add("hidden");
-            inputEmail = email // Kolla om denna funkar
         } else {
             alert(registerCustomer.msg)
         }
@@ -169,7 +127,7 @@ document.querySelector(".button").addEventListener("click", async (e) => {
                     return
                 }
 
-                alert(login.msg) 
+                alert("Välkommen tillbaka " + login.msg + "!") 
                 showCorrectAuthBoxes();
         } else {
             alert("Alla fält måste vara ifyllda")
--- client/validation/validation.js
+++ client/validation/validation.js
@@ -10,35 +10,36 @@ const validateValues = (object) => {
         object.address.country.length == 0 || 
         object.phone.length == 0
     ) {
-        return {bool: false, msg: "Fel!" }
+        return {bool: false, msg: "Fyll i alla fält!" }
     }
     if(!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(object.email)) {
-        return {bool: false, msg: "Fel!" }
+        return {bool: false, msg: "Email - Fel format" }
     }
     if(object.address.country == "Sverige" && object.address.postal_code.length != 5 || /^[0-9]+$/.test(object.address.postal_code) == false) {
-        return {bool: false, msg: "Fel!"}
+        return {bool: false, msg: "Postkod - Måste innehålla 5 siffror"}
     }
     if(object.address.country == "Norge" && object.address.postal_code.length != 4 || /^[0-9]+$/.test(object.address.postal_code) == false) {
-        return {bool: false, msg: "Fel!"}
+        return {bool: false, msg: "Postkod - Måste innehålla 4 siffror"}
     }
     if(object.phone.length != 10 || /^[0-9]+$/.test(object.phone) == false) {
-        return {bool: false, msg: "Fel!"}
+        return {bool: false, msg: "Telefonnummer -  Måste innehålla 10 siffror. Ex. 0721112233"}
     }
     if(object.password.length < 6) {
-        return {bool: false, msg: "Fel!"}
+        return {bool: false, msg: "Lösenord - Måste innehålla minst 6 karaktärer"}
     }
     if(object.name.length < 3 ) {
-        return {bool: false, msg: "Fel!"}
+        return {bool: false, msg: "Namn - Måste innehålla minst 2 bokstäver"}
     } 
     if(/^[A-Za-z\s\u00C0-\u00ff_-]*$/.test(object.name) == false){
-        return {bool: false, msg: "Fel!"}
+        return {bool: false, msg: "Namn - Får endast innehålla bokstäver"}
     }
     if(/^[A-Za-z\s\u00C0-\u00ff_-]*$/.test(object.address.city) == false){
-        return {bool: false, msg: "Fel!"}
+        return {bool: false, msg: "Ort - Får endast innehålla bokstäver"}
     }
     if(object.address.city.length < 3) {
-        return {bool: false, msg: "Fel!"}
+        return {bool: false, msg: "Ort - Måste innehålla minst 2 bokstäver"}
     }
+
     return {bool: true, msg: "Pass!"}
 }
 
